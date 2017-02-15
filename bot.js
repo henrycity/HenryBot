19873ae7dc130c9233fc9c8d1e9b215afa3214eb
Tri Tran <tvtri96@yahoo.com.vn>
2017-02-15 10:35:52 +0100

    Refractor let to const and update time, array

--- bot.js
+++ bot.js
@@ -1,21 +1,21 @@
-let request = require('superagent');
-let RtmClient = require('@slack/client').RtmClient;
-let RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const request = require('superagent');
+const RtmClient = require('@slack/client').RtmClient;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-let bot_token = process.env.BOT_API_KEY || '';
+const bot_token = process.env.BOT_API_KEY || '';
 
 let rtm = new RtmClient(bot_token);
 let lastRemindingTime;
 let remindingFirstTime = true;
 
 rtm.on(RTM_EVENTS.MESSAGE, function(data) {
-    let channel = data.channel;
+    const channel = data.channel;
     console.log(data);
-    let thread_ts = data.thread_ts;
+    const thread_ts = data.thread_ts;
     if ((channel[0] === 'C' || channel[0] === 'U') && !data.hasOwnProperty('subtype')) {
-        let text = data.text;
-        let API_URL = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages';
-        let message = {"documents": [{"id": "string", "text": text}]};
+        const text = data.text;
+        const API_URL = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages';
+        const message = {"documents": [{"id": "string", "text": text}]};
         request.post(API_URL)
             .type('application/json')
             .set('Ocp-Apim-Subscription-Key', process.env.LANG_API_KEY)
@@ -24,49 +24,55 @@ rtm.on(RTM_EVENTS.MESSAGE, function(data) {
                 if (err || !res.ok) {
                     console.log("Error", err);
                 } else {
-                    let detection = res.body.documents[0].detectedLanguages[0];
-                    let isFinnish = (detection.name === "Finnish" && detection.score > 0.8);
-                    let notSimpleFinnish = !text.includes('kiitos') || !text.includes('moi') || !text.includes('hyvää synttää') || !text.includes('hyvää synttäriä') || !text.includes('hyvää syntymäpäivää') || !text.includes('onnea');
+                    const detection = res.body.documents[0].detectedLanguages[0];
+                    const isFinnish = (detection.name === "Finnish" && detection.score > 0.8);
+                    const simpleFinnishTerms = ['kiitos', 'moi', 'hyvää synttää', 'hyvää synttäriä', 'hyvää syntymäpäivää', 'onnea'];
+                    let notSimpleFinnish = true;
+                    simpleFinnishTerms.forEach((term) => {
+                        if (text.includes(term)) {
+                            return notSimpleFinnish = false;
+                        }
+                    });
                     if (isFinnish && notSimpleFinnish) {
-                        let random = Math.floor((Math.random() * 5));
-                        let catchpharse = "";
+                        const random = Math.floor((Math.random() * 5));
+                        let catchphrase = "";
                         switch (random) {
                             case 0:
-                                catchpharse = "Tri is sad. Please speak English! #maketrihappyagain";
+                                catchphrase = "Tri is sad. Please speak English! #maketrihappyagain";
                                 break;
                             case 1:
-                                catchpharse = "Ei? Joo? Argghh, I cannot understand. Can you repeat that in English?";
+                                catchphrase = "Ei? Joo? Argghh, I cannot understand. Can you repeat that in English?";
                                 break;
                             case 2:
-                                catchpharse = "Tri maybe interested in joining the conversation? Can you say it again in English so that he can understand?";
+                                catchphrase = "Tri maybe interested in joining the conversation? Can you say it again in English so that he can understand?";
                                 break;
                             case 3: 
-                                catchpharse = "Please don't make Tri use Google Translate again :(";
+                                catchphrase = "Please don't make Tri use Google Translate again :(";
                                 break;
                             case 4:
-                                catchpharse = "Minä olen Henry. Minä don't speak Finnish.";
+                                catchphrase = "Minä olen Henry. Minä don't speak Finnish.";
                                 break;
                             default:
-                                catchpharse = "Tri is sad. Please speak English! #maketrihappyagain";
+                                catchphrase = "Tri is sad. Please speak English! #maketrihappyagain";
                                 break;
                         }
-                        let currentTime = new Date();
+                        const currentTime = new Date();
                         if (!lastRemindingTime) {
                             lastRemindingTime = new Date();
                         }
-                        let timePassed = Math.abs(currentTime - lastRemindingTime) / (1000 * 60 * 5);
-                        let canRemindAgain = timePassed >= 0.5;
+                        const timePassed = Math.abs(currentTime - lastRemindingTime) / (1000 * 60 * 5);
+                        const canRemindAgain = timePassed >= 1;
                         if (remindingFirstTime || canRemindAgain) {
                             if (thread_ts) {
 	                            rtm.send({
-		                            text: catchpharse,
+		                            text: catchphrase,
 		                            channel: channel,
 		                            thread_ts: thread_ts,
 		                            type: RTM_EVENTS.MESSAGE,
 	                            });
                             }
                             else {
-                                rtm.sendMessage(catchpharse, channel);
+                                rtm.sendMessage(catchphrase, channel);
                             }
                             remindingFirstTime = false;
                         }
